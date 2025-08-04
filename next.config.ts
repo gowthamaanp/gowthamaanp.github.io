1732a25a86d2a9bab7da8b819743483c3a3164d5
Gowthamaan <70436525+gowthamaanp@users.noreply.github.com>
2025-08-04 22:46:12 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   transpilePackages: ['next-mdx-remote'],
+  output: "export",  // <=== enables static exports
+  reactStrictMode: true,
 };
 
 export default nextConfig;
